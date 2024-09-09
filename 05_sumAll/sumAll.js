88353903d8d15a0718ca432ed74ef1676c908702
Ahmed Nader <130300856+ahmedn18@users.noreply.github.com>
2024-09-09 03:37:23 +0300

    Solbe 1, 2, 3, 4, 5 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || !Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    // Swap elements to be able to use gauss formula
+    if (start > end) [start, end] = [end, start];
 
+    // to avoid zero result (1 - 1) = 0
+    if (start > 1) return (end * start * (end + 1) * (start - 1) / 2);
+    
+    else return (end * (end + 1) / 2);
 };
 
 // Do not edit below this line
